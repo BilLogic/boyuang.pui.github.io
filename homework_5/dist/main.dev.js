4eb5a64f34e89a212fc65e077237caf85ac5da75
BilLogic <boyuang@andrew.cmu.edu>
2020-10-11 10:35:14 -0400

    More animation and a little adjustment to the alignment

--- homework_5/dist/main.dev.js
+++ homework_5/dist/main.dev.js
@@ -34,3 +34,30 @@ function openCart() {
 
   cartSwitch = !cartSwitch;
 }
+
+function openCart2() {
+  var cartLogo = document.getElementById("theCartLogo"); //console.log(cartLogo);
+
+  var menu = document.getElementById("mainMenu"); //console.log(menu);
+  //const layout = document.getElementById("GeneralLayout");
+
+  var cartList = document.getElementById("cartDisplay");
+
+  if (!cartSwitch) {
+    cartLogo.src = "../Imgs/Buttons/Cart_CheckOut.svg";
+    menu.style.gridTemplateAreas = "'roll_1 roll_2 roll_3 roll_4 .' 'roll_5 roll_6 roll_7 roll_8 .'";
+    menu.style.gridAutoColumns = "2fr 2fr 2fr 2fr 1fr"; //layout.style.gridTemplateAreas = "'header header openCart' 'content content 0penCart'";
+    //layout.style.gridTemplateColumns = "3fr 3fr 1fr";
+
+    cartList.style.display = "grid";
+  } else {
+    cartLogo.src = "../Imgs/CartBun_cart_icon.svg";
+    menu.style.gridTemplateAreas = "'roll_1 roll_2 roll_3 roll_4' 'roll_5 roll_6 roll_7 roll_8'";
+    menu.style.gridAutoColumns = "1fr 1fr 1fr 1fr"; //layout.style.gridTemplateAreas = "'header header header' 'content content content'";
+    //layout.style.gridTemplateColumns = "1fr 1fr 1fr";
+
+    cartList.style.display = "none";
+  }
+
+  cartSwitch = !cartSwitch;
+}
