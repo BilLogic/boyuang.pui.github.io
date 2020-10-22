c3465e1c690cf1ec21b8d229a8a8064b8fe46b74
BilLogic <boyuang@andrew.cmu.edu>
2020-10-21 21:46:44 -0400

    Implemented auto fill description for product description page

--- homework_6/dist/main.dev.js
+++ homework_6/dist/main.dev.js
@@ -1,5 +1,106 @@
 "use strict";
 
+// Roll Info constructor function
+function RollDescription(imgUrl, itemName, price, description, rate, calory, allergy) {
+  this.imgUrl = imgUrl;
+  this.itemName = itemName;
+  this.price = price;
+  this.description = description;
+  this.rate = rate;
+  this.calory = calory;
+  this.allergy = allergy;
+} // Roll description constants
+
+
+var ogDescription = "Homemade cinnamon roll with grandma's love.";
+var brDescription = "Homemade cinnamon roll with love. In addition, with self-planted blackberry inside!";
+var waDescription = "Homemade cinnamon roll with love. In addition, with walnut on the top!";
+var gfDescription = "Homemade cinnamon roll with love. In addition, it's gluten free!";
+var psDescription = "Homemade cinnamon roll with love. In addition, with pumpkin spice inside!";
+var cpDescription = "Homemade cinnamon roll with love. In addition, with caramel pecan on the top!"; // Roll Full Info constructor
+
+var defaultInfo = new RollDescription("Imgs/RollPics/count1.png", "Default Value", 0, "", 0.0, 0, false);
+var ogRollInfo = new RollDescription("../Imgs/RollPics/og-roll-pic.svg", "OG Roll", 1.5, ogDescription, 5.0, 560, true);
+var bbRollInfo = new RollDescription("../Imgs/RollPics/blackberry-roll-pic.svg", "Blackberry Roll", 2.0, brDescription, 4.5, 580, false);
+var waRollInfo = new RollDescription("../Imgs/RollPics/walnut-roll-pic.svg", "Walnut Roll", 2.5, waDescription, 4.7, 620, true);
+var gfRollInfo = new RollDescription("../Imgs/RollPics/gfree-roll-pic.svg", "Gluten Free Roll", 1.2, gfDescription, 5.0, 450, false);
+var psRollInfo = new RollDescription("../Imgs/RollPics/pumpkin-roll-pic.svg", "Pumpkin Spice Roll", 2, psDescription, 5, 560, true);
+var cpRollInfo = new RollDescription("../Imgs/RollPics/pecan-roll-pic.svg", "Caramel Pecan Roll", 3.0, cpDescription, 4.9, 650, true); // Array of built-in Roll Info
+
+var rollInfo = [defaultInfo, ogRollInfo, bbRollInfo, waRollInfo, gfRollInfo, psRollInfo, cpRollInfo]; // Function that parse the detail rollInfo into Product Description page
+
+function setDetailInfo(index) {
+  // get info object
+  var jsonSelectedRoll = JSON.stringify(rollInfo[index]); // store locally
+
+  localStorage.setItem("selectedRoll", jsonSelectedRoll);
+} // Helper function that add a img element under the given parentNode
+
+
+function AddImgTemplate(parentNode, src, alt) {
+  var id = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
+  var imgNode = document.createElement('img');
+  var source = document.createAttribute('src');
+  source.value = src;
+  var altValue = document.createAttribute('alt');
+  altValue.value = alt;
+
+  if (id != null) {
+    var idValue = document.createAttribute('id');
+    idValue.value = id;
+    imgNode.setAttributeNode(idValue);
+  }
+
+  imgNode.setAttributeNode(source);
+  imgNode.setAttributeNode(altValue);
+  parentNode.appendChild(imgNode);
+}
+
+function ContentTemplate(roll) {
+  console.log("ContentTemplate got called"); // get outer container
+
+  var bodyContainer = document.getElementById("description-section"); // create three sections divs
+
+  var picDiv = document.createElement("div");
+  picDiv.className = "pic";
+  var tagDiv = document.createElement("div");
+  tagDiv.className = "tag";
+  var infoDiv = document.createElement("div");
+  infoDiv.className = "info"; // fill in pic information
+
+  var leftArrow = document.createElement('div');
+  var rightArrow = document.createElement('div');
+  leftArrow.className = "left";
+  rightArrow.className = "right";
+  AddImgTemplate(leftArrow, "../Imgs/Buttons/huge-arrow.svg", "Switch to the previous item");
+  AddImgTemplate(rightArrow, "../Imgs/Buttons/huge-arrow.svg", "Switch to the previous item");
+  AddImgTemplate(picDiv, roll.imgUrl, "Roll Picture", "itemPic"); // fill in tag information
+
+  tagDiv.innerHTML = "Item: ".concat(roll.itemName, "<br/> \n                        Price: ").concat(roll.price); // fill in description information
+
+  infoDiv.innerHTML = "Flavor: ".concat(roll.description, "<br/>\n                        Calories: ").concat(roll.calory, "<br/>\n                        Popularity: ").concat(roll.rate, "<br/>\n                        Allergy: ").concat(roll.allergy ? "YES" : "NO"); // append created divs to the container
+
+  bodyContainer.appendChild(picDiv);
+  bodyContainer.appendChild(tagDiv);
+  bodyContainer.appendChild(infoDiv);
+  console.log(bodyContainer);
+} // get tag text from local storage
+
+
+function getInfo() {
+  console.log("getInfo got called");
+  var JsonSelectedRoll = localStorage.getItem("selectedRoll");
+
+  if (JsonSelectedRoll === null) {
+    console.log("got a problem when parsing the info!");
+  } else {
+    var parsedSelection = JSON.parse(JsonSelectedRoll);
+    console.log(parsedSelection);
+    console.log("Calling ContentTemplate");
+    ContentTemplate(parsedSelection);
+  }
+}
+
 function TurnOnItemPage() {
   var menu = document.getElementById("mainMenu");
   menu.style.display = "none";
--- homework_6/main.js
+++ homework_6/main.js
@@ -1,3 +1,127 @@
+
+// Roll Info constructor function
+function RollDescription(imgUrl, itemName, price, description, rate, calory, allergy){
+    this.imgUrl = imgUrl;
+    this.itemName = itemName;
+    this.price = price;
+    this.description = description;
+    this.rate = rate;
+    this.calory = calory;
+    this.allergy = allergy;
+}
+
+// Roll description constants
+const ogDescription = "Homemade cinnamon roll with grandma's love.";
+const brDescription = "Homemade cinnamon roll with love. In addition, with self-planted blackberry inside!";
+const waDescription = "Homemade cinnamon roll with love. In addition, with walnut on the top!";
+const gfDescription = "Homemade cinnamon roll with love. In addition, it's gluten free!";
+const psDescription = "Homemade cinnamon roll with love. In addition, with pumpkin spice inside!";
+const cpDescription = "Homemade cinnamon roll with love. In addition, with caramel pecan on the top!";
+
+// Roll Full Info constructor
+const defaultInfo = new RollDescription("Imgs/RollPics/count1.png", "Default Value", 0, "", 0.0, 0, false);
+const ogRollInfo = new RollDescription("../Imgs/RollPics/og-roll-pic.svg", "OG Roll", 1.5, ogDescription, 5.0, 560, true);
+const bbRollInfo = new RollDescription("../Imgs/RollPics/blackberry-roll-pic.svg", "Blackberry Roll", 2.0, brDescription, 4.5, 580, false);
+const waRollInfo = new RollDescription("../Imgs/RollPics/walnut-roll-pic.svg", "Walnut Roll", 2.5, waDescription, 4.7, 620, true);
+const gfRollInfo = new RollDescription("../Imgs/RollPics/gfree-roll-pic.svg", "Gluten Free Roll", 1.2, gfDescription, 5.0, 450, false);
+const psRollInfo = new RollDescription("../Imgs/RollPics/pumpkin-roll-pic.svg", "Pumpkin Spice Roll", 2, psDescription, 5, 560, true);
+const cpRollInfo = new RollDescription("../Imgs/RollPics/pecan-roll-pic.svg", "Caramel Pecan Roll", 3.0, cpDescription, 4.9, 650, true);
+
+
+// Array of built-in Roll Info
+const rollInfo = [defaultInfo, ogRollInfo, bbRollInfo, waRollInfo, gfRollInfo, psRollInfo, cpRollInfo];
+
+// Function that parse the detail rollInfo into Product Description page
+function setDetailInfo(index){
+    // get info object
+    const jsonSelectedRoll = JSON.stringify(rollInfo[index]);
+    // store locally
+    localStorage.setItem("selectedRoll", jsonSelectedRoll);
+}
+
+
+// Helper function that add a img element under the given parentNode
+function AddImgTemplate(parentNode, src, alt, id=null){
+    let imgNode = document.createElement('img');
+    const source = document.createAttribute('src')
+    source.value = src;
+    const altValue = document.createAttribute('alt');
+    altValue.value = alt;
+    
+    if(id != null){    
+        const idValue = document.createAttribute('id');
+        idValue.value = id;
+        imgNode.setAttributeNode(idValue);
+    }
+
+    imgNode.setAttributeNode(source);
+    imgNode.setAttributeNode(altValue);
+    parentNode.appendChild(imgNode);
+}
+
+
+function ContentTemplate(roll){
+    console.log("ContentTemplate got called");
+    // get outer container
+    const bodyContainer = document.getElementById("description-section");
+
+    // create three sections divs
+    let picDiv = document.createElement("div");
+    picDiv.className = "pic";
+    let tagDiv = document.createElement("div");
+    tagDiv.className ="tag";
+    let infoDiv = document.createElement("div");
+    infoDiv.className = "info";
+
+    // fill in pic information
+    let leftArrow = document.createElement('div');    
+    let rightArrow = document.createElement('div');   
+    leftArrow.className = "left";
+    rightArrow.className = "right";
+    AddImgTemplate(leftArrow, "../Imgs/Buttons/huge-arrow.svg", "Switch to the previous item");
+    AddImgTemplate(rightArrow, "../Imgs/Buttons/huge-arrow.svg", "Switch to the previous item");
+    AddImgTemplate(picDiv, roll.imgUrl, "Roll Picture", "itemPic");
+
+    // fill in tag information
+    tagDiv.innerHTML = `Item: ${roll.itemName}<br/> 
+                        Price: ${roll.price}`;
+
+    // fill in description information
+    infoDiv.innerHTML = `Flavor: ${roll.description}<br/>
+                        Calories: ${roll.calory}<br/>
+                        Popularity: ${roll.rate}<br/>
+                        Allergy: ${roll.allergy ? "YES" : "NO"}`
+
+
+    // append created divs to the container
+    bodyContainer.appendChild(picDiv);
+    bodyContainer.appendChild(tagDiv);
+    bodyContainer.appendChild(infoDiv);
+    console.log(bodyContainer);
+}
+
+
+// get tag text from local storage
+function getInfo(){
+    console.log("getInfo got called");
+    const JsonSelectedRoll = localStorage.getItem("selectedRoll");
+    if (JsonSelectedRoll === null){
+        console.log("got a problem when parsing the info!");
+    }
+    else {
+        const parsedSelection = JSON.parse(JsonSelectedRoll);
+        console.log(parsedSelection);
+        console.log("Calling ContentTemplate");
+        ContentTemplate(parsedSelection);
+    }
+}
+
+
+
+
+
+
+
 function TurnOnItemPage() {
     const menu = document.getElementById("mainMenu");
     menu.style.display = "none";
