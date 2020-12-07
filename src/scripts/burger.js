3cadac35911373c7d72b36a2fcc681c5f3fe8df8
BilLogic <boyuang@andrew.cmu.edu>
2020-12-07 12:59:41 -0500

    So cool

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,3 +1,94 @@
+// Google Map Api Parser
+function initMap() {
+    // Map options
+    let options = {
+        zoom: 5,
+        center: {lat:40.4433, lng: -79.9436}
+    }
+    // New map
+    let map = new google.maps.Map(document.getElementById('map'), options);
+
+    // listen for click on map
+    /*
+    google.maps.event.addListener(map, "click",
+        function (event) {
+            //add marker
+            addMarker({ coords: event.latLng });
+        }
+    );
+    */
+    
+    // Array of markers
+    let markers = [
+        {
+            coords: { lat: 40.4433, lng: -79.9436 },
+            content: "<h1>Demo Burger</h1>"
+        },
+        {
+        coords: { lat: 42, lng: -80.9436 },
+        content: "<h1>Demo Burger</h1>"
+    }
+    ]
+    
+    // loop through markers
+    markers.forEach(element => addMarker(element));
+
+    // Add Marker function
+    function addMarker(props) {
+        let marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+        });
+        // check for custom icon
+        if (props.iconImage) {
+            // set icon
+            marker.setIcon(props.iconImage);
+        }         
+        // check content
+        if (props.content) {
+            let infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+            marker.addListener("click", function () {
+                infoWindow.open(map, marker);
+            });
+        }
+    }
+}
+
+
+
+// Template Function
+function buildBurgerDescription() {
+    BURGERDATA.forEach(element => {
+        // Parse info into new object
+        let burger = new Burger(element.id, element.title, element.origin, element.img, element.restaurantImg, element.description);
+    });
+}
+
+/*************** Smooth Scroll Setting ***********/ 
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  lerp: 0.03,
+});
+
+// const testButton = document.querySelector("#testButton");
+// testButton.addEventListener("click", testClickHandler);
+
+/*************** In page Transitions ***********/
+/** 
+ * @brief Onclick => Scroll to Dest ID
+ * @args destID: #xxxxx
+*/
+function ScrollTo(destID) {
+    const dest = document.querySelector(destID);
+    locoScroll.scrollTo(dest);
+}
+
+
+
 // burger details info structure
 class Burger {
     constructor(id, title, origin, img, restaurantImg, description) {
@@ -17,62 +108,3 @@ class Burger {
 // Parsing data to Text Menu
 
 // Parsing data to slides
-
-// Template Function
-function buildBurgerDescription() {
-    BURGERDATA.forEach(element => {
-        // Parse info into new object
-        let burger = new Burger(element.id, element.title, element.origin, element.img, element.restaurantImg, element.description);
-    });
-}
-
-gsap.registerPlugin(ScrollTrigger);
-
-// gsap.to(".line-inner", {
-//   y: "0%",
-//   rotate: "0deg",
-//   duration: 0.7,
-//   delay: 0.5,
-//   stagger: {
-//     from: "end",
-//     amount: 0.1
-//   }
-// });
-
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-  lerp: 0.01,
-});
-
-// locoScroll.on("scroll", ScrollTrigger.update);
-
-// ScrollTrigger.scrollerProxy("body", {
-//   scrollTop(value) {
-//     return arguments.length
-//       ? locoScroll.scrollTo(value, 0, 0)
-//       : locoScroll.scroll.instance.scroll.y;
-//   },
-//   getBoundingClientRect() {
-//     return {
-//       top: 0,
-//       left: 0,
-//       width: window.innerWidth,
-//       height: window.innerHeight
-//     };
-//   }
-// });
-
-// gsap.to(".background", {
-//   backgroundColor: "red",
-//   duration: 1,
-//   ease: "Power4.easeIn",
-//   scrollTrigger: {
-//     start: "50% 50%",
-//     scrub: true,
-//     markers: true
-//   }
-// });
-
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-// ScrollTrigger.refresh();
--- src/scripts/dist/burger.dev.js
+++ src/scripts/dist/burger.dev.js
@@ -2,7 +2,100 @@
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-// burger details info structure
+// Google Map Api Parser
+function initMap() {
+  // Map options
+  var options = {
+    zoom: 5,
+    center: {
+      lat: 40.4433,
+      lng: -79.9436
+    }
+  }; // New map
+
+  var map = new google.maps.Map(document.getElementById('map'), options); // listen for click on map
+
+  /*
+  google.maps.event.addListener(map, "click",
+      function (event) {
+          //add marker
+          addMarker({ coords: event.latLng });
+      }
+  );
+  */
+  // Array of markers
+
+  var markers = [{
+    coords: {
+      lat: 40.4433,
+      lng: -79.9436
+    },
+    content: "<h1>Demo Burger</h1>"
+  }, {
+    coords: {
+      lat: 42,
+      lng: -80.9436
+    },
+    content: "<h1>Demo Burger</h1>"
+  }]; // loop through markers
+
+  markers.forEach(function (element) {
+    return addMarker(element);
+  }); // Add Marker function
+
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+      position: props.coords,
+      map: map
+    }); // check for custom icon
+
+    if (props.iconImage) {
+      // set icon
+      marker.setIcon(props.iconImage);
+    } // check content
+
+
+    if (props.content) {
+      var infoWindow = new google.maps.InfoWindow({
+        content: props.content
+      });
+      marker.addListener("click", function () {
+        infoWindow.open(map, marker);
+      });
+    }
+  }
+} // Template Function
+
+
+function buildBurgerDescription() {
+  BURGERDATA.forEach(function (element) {
+    // Parse info into new object
+    var burger = new Burger(element.id, element.title, element.origin, element.img, element.restaurantImg, element.description);
+  });
+}
+/*************** Smooth Scroll Setting ***********/
+
+
+var locoScroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  lerp: 0.03
+}); // const testButton = document.querySelector("#testButton");
+// testButton.addEventListener("click", testClickHandler);
+
+/*************** In page Transitions ***********/
+
+/** 
+ * @brief Onclick => Scroll to Dest ID
+ * @args destID: #xxxxx
+*/
+
+function ScrollTo(destID) {
+  var dest = document.querySelector(destID);
+  locoScroll.scrollTo(dest);
+} // burger details info structure
+
+
 var Burger = function Burger(id, title, origin, img, restaurantImg, description) {
   _classCallCheck(this, Burger);
 
@@ -16,56 +109,3 @@ var Burger = function Burger(id, title, origin, img, restaurantImg, description)
 // Parsing data to Map Menu
 // Parsing data to Text Menu
 // Parsing data to slides
-// Template Function
-
-
-function buildBurgerDescription() {
-  BURGERDATA.forEach(function (element) {
-    // Parse info into new object
-    var burger = new Burger(element.id, element.title, element.origin, element.img, element.restaurantImg, element.description);
-  });
-}
-
-gsap.registerPlugin(ScrollTrigger); // gsap.to(".line-inner", {
-//   y: "0%",
-//   rotate: "0deg",
-//   duration: 0.7,
-//   delay: 0.5,
-//   stagger: {
-//     from: "end",
-//     amount: 0.1
-//   }
-// });
-
-var locoScroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-  lerp: 0.01
-}); // locoScroll.on("scroll", ScrollTrigger.update);
-// ScrollTrigger.scrollerProxy("body", {
-//   scrollTop(value) {
-//     return arguments.length
-//       ? locoScroll.scrollTo(value, 0, 0)
-//       : locoScroll.scroll.instance.scroll.y;
-//   },
-//   getBoundingClientRect() {
-//     return {
-//       top: 0,
-//       left: 0,
-//       width: window.innerWidth,
-//       height: window.innerHeight
-//     };
-//   }
-// });
-// gsap.to(".background", {
-//   backgroundColor: "red",
-//   duration: 1,
-//   ease: "Power4.easeIn",
-//   scrollTrigger: {
-//     start: "50% 50%",
-//     scrub: true,
-//     markers: true
-//   }
-// });
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-// ScrollTrigger.refresh();
